7f11a5cc04e2787994c303312063835a1d29e30e
Johann Sylvester <johannsylvester04@gmail.com>
2023-03-07 11:25:42 +0530

    Initialized grid.

--- game_of_life.c
+++ game_of_life.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+#define HSIZE 100
+#define VSIZE 100
+
 int main()
 {
-	printf("Hello world!");
+	int i, j;
+	int grid[HSIZE][VSIZE];
+
+	for (i = 0; i < HSIZE; i++)
+		for (j = 0; j < VSIZE; j++)
+			grid[i][j] = 0;
 }
